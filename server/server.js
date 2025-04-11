e824efc81e405cd2c8481eca62ced877c5b6ab4b
dhara-maru <dharamaru406@gmail.com>
2025-04-11 16:47:52 +0530

    changed the pwd

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect('mongodb+srv://rekhta-mern:rekhtamerndhara@cluster0.s3zyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://dharaMERN:dharaMERN@cluster0.xhmws.mongodb.net/unsentproject?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
